0c7fe0ddb9fb9599e890ddc63739d4b14e7ad699
Harrison Asmar <harrisonasmar@gmail.com>
2023-06-10 12:35:00 +1000

    Created board test code
    Allows for testing of the button and all the LEDs at once. Also outputs to the Serial Monitor in case LEDs are not working.

---
+++ boardTest/boardTest.ino
@@ -0,0 +1,56 @@
+/*
+  Scouts-Soldering-Challenge Board Test
+
+  The button enables the LEDs on the board, and all LEDs should light up when the button is pressed. 
+
+  Based off the Arduino Example "Button"
+  Created by Harrison "Turtle" Asmar
+
+  Version 1 - 10/06/2023
+*/
+
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;  // the number of the pushbutton pin
+const int ledPin1 = 13;    // the number of the LED pin
+const int ledPin2 = 12;    // the number of the LED pin
+const int ledPin3 = 11;    // the number of the LED pin
+const int ledPin4 = 10;    // the number of the LED pin
+const int ledPin5 = 9;    // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;  // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    digitalWrite(ledPin5, HIGH);
+    Serial.write("HIGH");
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin4, LOW);
+    digitalWrite(ledPin5, LOW);
+  }
+}
