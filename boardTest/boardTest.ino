e526b32914b613596c09ef7923fae560948258c1
Harrison Asmar <harrison@crankshafttech.com>
2023-06-20 14:16:49 +1000

    Updated boardTest
    Updated boardTest to make variable names match silkscreened values on PCB.

--- boardTest/boardTest.ino
+++ boardTest/boardTest.ino
@@ -11,22 +11,22 @@
 
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin = 2;  // the number of the pushbutton pin
-const int ledPin1 = 13;    // the number of the LED pin
-const int ledPin2 = 12;    // the number of the LED pin
-const int ledPin3 = 11;    // the number of the LED pin
-const int ledPin4 = 10;    // the number of the LED pin
-const int ledPin5 = 9;    // the number of the LED pin
+const int ledRed = 13;    // the number of the LED pin
+const int ledYellow = 12;    // the number of the LED pin
+const int ledGreen = 11;    // the number of the LED pin
+const int ledMaroon = 10;    // the number of the LED pin
+const int ledOrange = 9;    // the number of the LED pin
 
 // variables will change:
 int buttonState = 0;  // variable for reading the pushbutton status
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
-  pinMode(ledPin4, OUTPUT);
-  pinMode(ledPin5, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledMaroon, OUTPUT);
+  pinMode(ledOrange, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
   Serial.begin(9600);
@@ -39,18 +39,18 @@ void loop() {
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
     // turn LED on:
-    digitalWrite(ledPin1, HIGH);
-    digitalWrite(ledPin2, HIGH);
-    digitalWrite(ledPin3, HIGH);
-    digitalWrite(ledPin4, HIGH);
-    digitalWrite(ledPin5, HIGH);
+    digitalWrite(ledRed, HIGH);
+    digitalWrite(ledYellow, HIGH);
+    digitalWrite(ledGreen, HIGH);
+    digitalWrite(ledMaroon, HIGH);
+    digitalWrite(ledOrange, HIGH);
     Serial.println("HIGH");
   } else {
     // turn LED off:
-    digitalWrite(ledPin1, LOW);
-    digitalWrite(ledPin2, LOW);
-    digitalWrite(ledPin3, LOW);
-    digitalWrite(ledPin4, LOW);
-    digitalWrite(ledPin5, LOW);
+    digitalWrite(ledRed, LOW);
+    digitalWrite(ledYellow, LOW);
+    digitalWrite(ledGreen, LOW);
+    digitalWrite(ledMaroon, LOW);
+    digitalWrite(ledOrange, LOW);
   }
 }
